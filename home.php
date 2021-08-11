7c043c3ba2d218b9c54bebe9b17903222482239a
vcfggit <88759194+vcfggit@users.noreply.github.com>
2021-08-11 10:23:48 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+	include ('Db.php');
+?>
+<?php
+	include('header.php');
+	include('body_top.php');
+	include('navigation.php');
+?>
+
+<h1 class="page-header">Home</h1>
+
+<?php
+	include('footer.php');
+?>
+
